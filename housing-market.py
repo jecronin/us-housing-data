119377026cfcf81efd913fd2ee2732959aa71681
jecronin <45111583+jecronin@users.noreply.github.com>
2022-11-15 17:46:58 -0500

    Update housing-market.py

--- housing-market.py
+++ housing-market.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 url = "https://econdata.s3-us-west-2.amazonaws.com/Reports/Core/RDC_Inventory_Core_Metrics_Zip_History.csv"
 cols = ['month_date_yyyymm', 'postal_code','median_listing_price',  'active_listing_count','median_days_on_market'] #add back zip name when want to use
 #data_dic = {'month_date_yyyymm':'string', 'postal_code':'string', 'zip_name':'string','median_listing_price':'int64',  'active_listing_count':'int32','median_days_on_market':'int32'}
-df = pd.read_csv(url, low_memory=False, usecols=cols)
+df = pd.read_csv(url, low_memory=False, usecols=cols, sep=',')
 df.drop(df.tail(1).index,inplace=True) # drop last row that has data RDC contact info                                 
 tgt_zips = ['74728', '94123', '11211', '11249', '30560', '39110', '95670'] 
 df = df[df.postal_code.isin(tgt_zips)]
