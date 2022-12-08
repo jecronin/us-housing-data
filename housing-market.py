a45354749235405920a699f938d7e5c86d5bde54
jecronin <45111583+jecronin@users.noreply.github.com>
2022-12-08 15:24:46 -0500

    Update housing-market.py

--- housing-market.py
+++ housing-market.py
@@ -73,7 +73,7 @@ df_tgt = df[df['postal_code'] == zip_input].sort_values('month_date_yyyymm', asc
 fig = px.line(df_tgt,
                 x='month_date_yyyymm',
                 y='median_listing_price',
-                title = 'Median Listing Price' + " in " + zip_input,
+                title = 'Median Listing Price' + " in " + zip_input + df_tgt['zip_name'].iloc[0],
                 markers=True
 )
 
