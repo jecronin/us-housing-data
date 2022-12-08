117fc92862630555f564a81721d1de5e988c4a25
jecronin <45111583+jecronin@users.noreply.github.com>
2022-12-08 10:24:44 -0500

    Update housing-market.py

--- housing-market.py
+++ housing-market.py
@@ -11,45 +11,44 @@ cols = ['month_date_yyyymm', 'postal_code','median_listing_price',  'active_list
 def load_data():
     d = pd.read_csv(url, low_memory=False, usecols=cols, sep=',')[:-1] #read in csv
     d['month_date_yyyymm'] = pd.to_datetime(d['month_date_yyyymm'], format='%Y%m') #convert date to datetime
+    #reduce memory of dataframe
+    def reduce_mem_usage(d):
+        for col in d.columns:
+            col_type = d[col].dtype
+        if col_type != object:
+                c_min = d[col].min()
+                c_max = d[col].max()
+                if str(col_type)[:3] == 'int':
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        d[col] = d[col].astype(np.int8)
+                    elif c_min > np.iinfo(np.uint8).min and c_max < np.iinfo(np.uint8).max:
+                        d[col] = d[col].astype(np.uint8)
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        d[col] = d[col].astype(np.int16)
+                    elif c_min > np.iinfo(np.uint16).min and c_max < np.iinfo(np.uint16).max:
+                        d[col] = d[col].astype(np.uint16)
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        d[col] = d[col].astype(np.int32)
+                    elif c_min > np.iinfo(np.uint32).min and c_max < np.iinfo(np.uint32).max:
+                        d[col] = d[col].astype(np.uint32)                    
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        d[col] = d[col].astype(np.int64)
+                    elif c_min > np.iinfo(np.uint64).min and c_max < np.iinfo(np.uint64).max:
+                        d[col] = d[col].astype(np.uint64)
+                elif str(col_type)[:5] == 'float':
+                    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        d[col] = d[col].astype(np.float16)
+                    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        d[col] = d[col].astype(np.float32)
+                    else:
+                        d[col] = d[col].astype(np.float64)
+    reduce_mem_usage(d)
     return d
 df = load_data()
 #data.drop(data.tail(1).index,inplace=True) # drop last row that has data RDC contact info                                 
 #tgt_zips = sorted(['74728', '94123', '11211', '11249', '30560', '39110', '95670', '35004', '35007', '35094', '12758', '37738', '37862', '12440'])#set target list of zips
 #df = data[data.postal_code.isin(tgt_zips)] #filter df
 #del data #delete large dataframe to save memory
-
-'''#reduce memory of dataframe
-def reduce_mem_usage(df):
-    for col in df.columns:
-        col_type = df[col].dtype
-    if col_type != object:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == 'int':
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.uint8).min and c_max < np.iinfo(np.uint8).max:
-                    df[col] = df[col].astype(np.uint8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.uint16).min and c_max < np.iinfo(np.uint16).max:
-                    df[col] = df[col].astype(np.uint16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.uint32).min and c_max < np.iinfo(np.uint32).max:
-                    df[col] = df[col].astype(np.uint32)                    
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)
-                elif c_min > np.iinfo(np.uint64).min and c_max < np.iinfo(np.uint64).max:
-                    df[col] = df[col].astype(np.uint64)
-            elif str(col_type)[:5] == 'float':
-                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-                    df[col] = df[col].astype(np.float16)
-                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
-                    df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
-reduce_mem_usage(df)'''
 
 #Create 3 columns
 col1, col2, col3 = st.columns([5, 5, 20])
