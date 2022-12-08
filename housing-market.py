398823f84836ca7e9310cd4f6e0cab21d6097cbe
jecronin <45111583+jecronin@users.noreply.github.com>
2022-12-08 10:19:38 -0500

    Update housing-market.py

--- housing-market.py
+++ housing-market.py
@@ -9,13 +9,15 @@ cols = ['month_date_yyyymm', 'postal_code','median_listing_price',  'active_list
 #data_dic = {'month_date_yyyymm':'string', 'postal_code':'string', 'zip_name':'string','median_listing_price':'int64',  'active_listing_count':'int32','median_days_on_market':'int32'}
 @st.cache
 def load_data():
-    return pd.read_csv(url, low_memory=False, usecols=cols, sep=',')[:-1] #read in csv
+    d = pd.read_csv(url, low_memory=False, usecols=cols, sep=',')[:-1] #read in csv
+    d['month_date_yyyymm'] = pd.to_datetime(d['month_date_yyyymm'], format='%Y%m') #convert date to datetime
+    return d
 df = load_data()
 #data.drop(data.tail(1).index,inplace=True) # drop last row that has data RDC contact info                                 
 #tgt_zips = sorted(['74728', '94123', '11211', '11249', '30560', '39110', '95670', '35004', '35007', '35094', '12758', '37738', '37862', '12440'])#set target list of zips
 #df = data[data.postal_code.isin(tgt_zips)] #filter df
 #del data #delete large dataframe to save memory
-df['month_date_yyyymm'] = pd.to_datetime(df['month_date_yyyymm'], format='%Y%m') #convert date to datetime
+
 #reduce memory of dataframe
 def reduce_mem_usage(df):
     for col in df.columns:
