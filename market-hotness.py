97fc2e86af31435a7ae079d3ce146bbf759334b4
jecronin <45111583+jecronin@users.noreply.github.com>
2022-12-16 10:02:45 -0800

    Update market-hotness.py

--- market-hotness.py
+++ market-hotness.py
@@ -15,7 +15,6 @@ def load_data():
     d = pd.read_csv(url, low_memory=False, usecols=cols, sep=',')[:-1] #read in csv and drop the last row of contact information
     d['month_date_yyyymm'] = pd.to_datetime(d['month_date_yyyymm'], format='%Y%m') #convert date to datetime
     d = d.drop_duplicates()
-    d['state'] = df['zip_name'].str[-2:]
     for col in ['hotness_rank_mm', 'hotness_rank_yy']:
        df[col] = df[col].fillna(0)    
 #reduce memory of dataframe
