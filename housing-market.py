3dec6bd5d9a846884cbaec1c5db480b1b38a063c
jecronin <45111583+jecronin@users.noreply.github.com>
2025-04-01 15:16:30 -0400

    Update housing-market.py

--- housing-market.py
+++ housing-market.py
@@ -69,21 +69,20 @@ data = {
 }
 st.table(pd.DataFrame(data))
 
+# Preview of data
+st.subheader("Preview of Realtor.com Housing Data")
+st.dataframe(df.head(25))
 
 # Filter data for selected ZIP
 df_tgt = df[df['postal_code'] == zip_input].sort_values('month_date_yyyymm')
 
-# Preview of data
-st.subheader("Preview of Realtor.com Housing Data")
-st.dataframe(df_tgt.head(25))
-
-# Plot each chart
+# Plotting function
 def plot_chart(data, x, y, title):
-    fig = px.line(data, x=x, y=y, title=title, markers=True)
-    st.plotly_chart(fig, use_container_width=True)
-
-# Create charts
-plot_chart(df_tgt, 'month_date_yyyymm', 'median_listing_price', 'Median Listing Price in ' + zip_input)
+    if y in data.columns and not data.empty:
+        fig = px.line(data, x=x, y=data[y].astype(float), title=title, markers=True)
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        st.warning(f"No data available for {title}")
 
 # Define charts to render
 charts = {
